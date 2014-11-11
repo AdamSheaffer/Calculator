ee37492e2e4e0cabbbd84946b05afc68d683653e
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2014-11-11 11:59:33 -0600

    added html file and basic styling

--- main.js
+++ main.js
@@ -1,53 +1,61 @@
-function displayOutput(){
-  return $('#display-screen').val()
-}
+$(document).ready(function(){
 
-// 4 + 6 + 8 =
+  $('#display-screen').text('0');
 
-var currentNumber = '';
-var newNumber = '';
-
-function press(buttonValue){
-
-  function calculate(){
-    newNumber = (currentNumber + buttonValue);
-    currentNumber = '';
-
-    $('#display-screen').val(newNumber);
+  function displayOutput(){
+    return $('#display-screen').val();
   }
 
-  switch(buttonValue) {
-    case '+':
-      calculate();
-      break;
-    case '-':
-      calculate();
-      break;
-    case '*':
-      calculate();
-      break;
-    case '/':
-      calculate();
-      break;
-    case '=':
-      newNumber = eval(currentNumber).toString();
-      $('#display-screen').val(newNumber.substring(0,10) * 1);
-      //currentNumber = newNumber;
-      break;
-    case 'C':
-      // clear
-    case '+/-':
-      //sign
-      break;
-    case '.':
-    //  if(currentNumber === undefined || currentNumber.indexOf('.') === -1){
-        currentNumber = $('#display-screen').val();
-        $('#display-screen').val(currentNumber += buttonValue);
-    //  }
-      break;
-    default:
-      console.log(buttonValue);
-      currentNumber = $('#display-screen').val() + buttonValue
-      $('#display-screen').val(currentNumber);
+  $("#numbers > a").not("#clear").click(function(){
+    press($(this).text());
+    $('#display-screen').text(number);
+  });
+
+  var currentNumber = '';
+  var newNumber = '';
+
+  function press(buttonValue){
+
+    function calculate(){
+      newNumber = (currentNumber + buttonValue);
+      currentNumber = '';
+
+      $('#display-screen').val(newNumber);
+    }
+
+    switch(buttonValue) {
+      case '+':
+        calculate();
+        break;
+      case '-':
+        calculate();
+        break;
+      case '*':
+        calculate();
+        break;
+      case '/':
+        calculate();
+        break;
+      case '=':
+        newNumber = eval(currentNumber).toString();
+        $('#display-screen').val(newNumber.substring(0,10) * 1);
+        //currentNumber = newNumber;
+        break;
+      case 'C':
+        // clear
+      case '+/-':
+        //sign
+        break;
+      case '.':
+      //  if(currentNumber === undefined || currentNumber.indexOf('.') === -1){
+          currentNumber = $('#display-screen').val();
+          $('#display-screen').val(currentNumber += buttonValue);
+      //  }
+        break;
+      default:
+        console.log(buttonValue);
+        currentNumber = $('#display-screen').val() + buttonValue;
+        $('#display-screen').val(currentNumber);
+    }
   }
-}
+});
